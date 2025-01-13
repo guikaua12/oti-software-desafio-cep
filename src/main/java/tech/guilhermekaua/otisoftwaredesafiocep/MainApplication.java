7820f5c9aee8a42e0cd94f3f85a91c9bf5bde148
Approximations <guilhermeksilva556@gmail.com>
2025-01-13 17:24:27 -0300

    chore: change main class name

--- src/main/java/tech/guilhermekaua/otisoftwaredesafiocep/MainApplication.java
+++ src/main/java/tech/guilhermekaua/otisoftwaredesafiocep/MainApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OtiSoftwareDesafioCepApplication {
+public class MainApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(OtiSoftwareDesafioCepApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
 
 }
