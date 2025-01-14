2330db8e3e2ea7ddb9be72f65177c10aab280af0
Approximations <guilhermeksilva556@gmail.com>
2025-01-13 22:53:26 -0300

    feat: added swagger documentation to CepController#findByCep

--- src/main/java/tech/guilhermekaua/otisoftwaredesafiocep/controllers/CepController.java
+++ src/main/java/tech/guilhermekaua/otisoftwaredesafiocep/controllers/CepController.java
@@ -1,5 +1,8 @@
 package tech.guilhermekaua.otisoftwaredesafiocep.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,7 +23,13 @@ public class CepController {
         this.cepService = cepService;
     }
 
+    @Operation(summary = "Retorna um CEP pelo valor dele")
     @GetMapping("/{cep}")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "CEP encontrado."),
+            @ApiResponse(responseCode = "404", description = "CEP não encontrado."),
+            @ApiResponse(responseCode = "400", description = "Erro de validação.")
+    })
     public CEP findByCep(@PathVariable @Cep String cep) {
         return cepService.findByCep(cep);
     }
