eff7e617d7d770712b758d3d74a7611d770a12b2
Approximations <guilhermeksilva556@gmail.com>
2025-01-14 18:34:49 -0300

    feat: generate CEP equals and hashCode

--- src/main/java/tech/guilhermekaua/otisoftwaredesafiocep/entities/CEP.java
+++ src/main/java/tech/guilhermekaua/otisoftwaredesafiocep/entities/CEP.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Objects;
+
 @Getter
 @Setter
 @Entity
@@ -26,5 +28,17 @@ public class CEP {
     }
 
     public CEP() {
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        CEP cep = (CEP) o;
+        return Objects.equals(id, cep.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
     }
 }
