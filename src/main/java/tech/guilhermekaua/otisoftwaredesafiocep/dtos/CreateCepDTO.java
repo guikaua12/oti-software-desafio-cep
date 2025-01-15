0bc6e25f3cc7f3bc0a047b48c8c2722ef0e1bcf3
Approximations <guilhermeksilva556@gmail.com>
2025-01-14 22:31:30 -0300

    feat: increase `CreateCepDTO#estado` maximum size to 32

--- src/main/java/tech/guilhermekaua/otisoftwaredesafiocep/dtos/CreateCepDTO.java
+++ src/main/java/tech/guilhermekaua/otisoftwaredesafiocep/dtos/CreateCepDTO.java
@@ -8,5 +8,5 @@ public record CreateCepDTO(@Cep String cep,
                            @NotBlank(message = "O logradouro é obrigatório.") @Size(max = 255, message = "O logradouro deve ter no máximo 255 caracteres.") String logradouro,
                            @NotBlank(message = "O bairro é obrigatório.") @Size(max = 255, message = "O bairro deve ter no máximo 255 caracteres.") String bairro,
                            @NotBlank(message = "A cidade é obrigatória.") @Size(max = 32, message = "A cidade deve ter no máximo 32 caracteres.") String cidade,
-                           @NotBlank(message = "O estado é obrigatório.") @Size(max = 16, message = "O estado deve ter no máximo 16 caracteres.") String estado) {
+                           @NotBlank(message = "O estado é obrigatório.") @Size(max = 16, message = "O estado deve ter no máximo 32 caracteres.") String estado) {
 }
